8c69cd0d882f73c23f53df1b621e72ad44991b44
karanhimadri <karanhimadri1234@gmail.com>
2025-06-03 06:25:02 +0530

    V2

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // Optional: if you don’t need `.mdx` pages, remove the extension
   pageExtensions: ['js', 'jsx'],
-
-  // Remove MDX-related experimental flags and webpack config
 };
 
 export default nextConfig;
